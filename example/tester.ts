fc38d589891bb255893da9789e4421cec2cb347e
Vitaly Turovsky <vital2580@icloud.com>
2025-02-13 21:35:51 +0300

    format

--- example/tester.ts
+++ example/tester.ts
@@ -66,44 +66,44 @@ bot.on('resourcePack', (url) => {
 })
 
 let block
-    onReady(bot).then(() => {
-        // Add interactive controls
-        bot.webViewer.ui.updateLil('controls', {
-            forward: false,
-            backwards: false,
-            left: false,
-            right: false,
-            sneak: false,
-            async jump() {
-                await bot.setControlState('jump', true)
-                setTimeout(() => bot.setControlState('jump', false), 100)
-            },
-            chat() {
-                bot.chat('Hello')
-            },
-            lookAtGrass() {
-                block = bot.findBlock({
-                    matching(b) {
-                        return b.name.includes('grass')
-                    }
-                })
-                console.log('Now looking at', block.name)
-                bot.lookAt(block.position)
-            },
-            lookAtBlockBelow() {
-                block = bot.world.getBlock(bot.entity.position.offset(0, -1, 0))
-                console.log('Now looking at', block.name)
-                bot.lookAt(block.position)
-            },
-            startDigging() {
-                bot.dig(block)
-            },
-            stopDigging() {
-                bot.stopDigging()
-            },
-            activateItem() {
-                bot.activateItem()
-            },
+onReady(bot).then(() => {
+    // Add interactive controls
+    bot.webViewer.ui.updateLil('controls', {
+        forward: false,
+        backwards: false,
+        left: false,
+        right: false,
+        sneak: false,
+        async jump() {
+            await bot.setControlState('jump', true)
+            setTimeout(() => bot.setControlState('jump', false), 100)
+        },
+        chat() {
+            bot.chat('Hello')
+        },
+        lookAtGrass() {
+            block = bot.findBlock({
+                matching(b) {
+                    return b.name.includes('grass')
+                }
+            })
+            console.log('Now looking at', block.name)
+            bot.lookAt(block.position)
+        },
+        lookAtBlockBelow() {
+            block = bot.world.getBlock(bot.entity.position.offset(0, -1, 0))
+            console.log('Now looking at', block.name)
+            bot.lookAt(block.position)
+        },
+        startDigging() {
+            bot.dig(block)
+        },
+        stopDigging() {
+            bot.stopDigging()
+        },
+        activateItem() {
+            bot.activateItem()
+        },
         lookRight() {
             bot.look(bot.entity.yaw - 1, bot.entity.pitch)
         },
@@ -116,25 +116,25 @@ let block
         lookDown() {
             bot.look(bot.entity.yaw, bot.entity.pitch - 1)
         },
-            // placeBlock() {
-            //     // bot.placeBlock(block)
-            // }
-        }, {
-            // Optional callback when values change
-            onUpdate(id, newValue) {
-                if (id === 'forward') {
-                    bot.setControlState('forward', newValue)
-                } else if (id === 'backwards') {
-                    bot.setControlState('back', newValue)
-                } else if (id === 'left') {
-                    bot.setControlState('left', newValue)
-                } else if (id === 'right') {
-                    bot.setControlState('right', newValue)
-                } else if (id === 'sneak') {
-                    bot.setControlState('sneak', newValue)
-                }
+        // placeBlock() {
+        //     // bot.placeBlock(block)
+        // }
+    }, {
+        // Optional callback when values change
+        onUpdate(id, newValue) {
+            if (id === 'forward') {
+                bot.setControlState('forward', newValue)
+            } else if (id === 'backwards') {
+                bot.setControlState('back', newValue)
+            } else if (id === 'left') {
+                bot.setControlState('left', newValue)
+            } else if (id === 'right') {
+                bot.setControlState('right', newValue)
+            } else if (id === 'sneak') {
+                bot.setControlState('sneak', newValue)
             }
-        })
+        }
+    })
     // Track connections
     const { _tcpServer, _wsServer } = bot.webViewer
 
